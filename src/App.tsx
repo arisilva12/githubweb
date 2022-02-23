89b9d2c2cc8bb1d9f85030def8019b4d9a2565f5
Aristides da Silva Pires <ari_silva1295@hotmail.com>
2022-02-23 14:04:08 -0300

    erro Switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
+import Routes from './routes';
 
-function App() {
-  return (
-    <h1>Hello Work</h1>
-  );
-}
+const App: React.FC = () => <Routes />
+
 
 export default App;
