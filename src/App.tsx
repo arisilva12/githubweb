d8fd8cfe89ee98bf02014f02be8c825b0e5cbf92
Aristides da Silva Pires <ari_silva1295@hotmail.com>
2022-02-24 13:56:47 -0300

    curso

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import Routes from './routes';
 
-const App: React.FC = () => <Routes />
-
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes path="/" exact component={Dashboard} />
+    <Routes path="/repository" component={Repository} />
+  </BrowserRouter>
+);
 
 export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,9 +5,10 @@ import Dashboard from '../pages/Dashboad';
 import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
-  <Switch>
-    <Route path="/" component={Dashboard} />
-  </Switch>
+  <Switch >
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repository} />
+  </Switch >
 )
 
-export default Routes;
+export default Routes; 
