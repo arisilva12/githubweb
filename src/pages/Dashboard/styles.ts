fc4488ba9640c094e32cd478d7c7cc75239d3da8
Aristides da Silva Pires <ari_silva1295@hotmail.com>
2022-03-01 17:13:08 -0300

    style

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,6 @@
-import { Style } from "styled-components";
+import { Styled } from "styled-components";
 
-const Title = Styled.h1`
+export const Title = Styled.h1`
 font-size: 48px;
 color: #3A3A3A;
 `;
